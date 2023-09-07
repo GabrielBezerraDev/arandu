a739bb565ed727fb9e828ca88b6a0815e551df76
SwattPK <bezerrag363@gmail.com>
2023-09-07 17:23:18 -0400

    [UPDATE] - Checando como ficou a pagina

--- script_ladingPage.js
+++ script_ladingPage.js
@@ -2,6 +2,14 @@ const iconMenu = document.querySelector(".icon-menu");
 const menu = document.querySelector(".menu");
 const body = document.querySelector("body");
 
+function defineImg(){
+    const images = document.querySelectorAll(".profileImg");
+    for(let image of images){
+        image.style.height = image.clientWidth;
+    }
+}
+defineImg();
+
 console.dir(menu);
 iconMenu.addEventListener("click", () => {
     menu.classList.toggle("showMenu");
