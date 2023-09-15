57eff194e9a93ab352f1eca7716a0fae607e69e3
SwattPK <bezerrag363@gmail.com>
2023-09-14 21:18:58 -0400

    Adicionando os perfis

--- script_ladingPage.js
+++ script_ladingPage.js
@@ -3,3 +3,16 @@ function redirect(url){
     window.open(url, '_blank');
 }
 
+
+const oberserver = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if(entry.isIntersecting){
+            entry.target.classList.add("show");
+            entry.target.classList.remove(".hidden");
+            console.log(entry.target);
+        }
+    });
+});
+
+const hiddensElements = document.querySelectorAll(".hidden");
+hiddensElements.forEach(el => oberserver.observe(el));
